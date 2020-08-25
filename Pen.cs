ad1e9f992290b57d4aaa9c9dc24e22064bf11fcf
Jia <jiavon@ualberta.ca>
2020-08-25 17:30:02 -0600

    working pen object that outputs percentage of pen ink left

--- Pen.cs
+++ Pen.cs
@@ -7,13 +7,13 @@ namespace OPP_Practice
     //Create a 'Pen' class that has the following properties:
     class Pen
     {
-        public double characterCount;
-        //properties to be created is Brand, InkColor, InkLevel, MaxInk, HasLid
+
         public string Brand { get; set; } //Brand is a string cause it is just names
         public string InkColor { get; set; } //Colour is a string because it is just names
 
 
         //InkLevel (Percentage, must not accept values below 0 or above 100, throw an exception if an attempt to go lower than 0 is made, and clamp values higher than 100 to 100. Round inputs to two decimal places.) - make the setter private, and make the getter round the output to the nearest 5%
+
         //we have to specify a backing variable for InkLevel because we are doing some validation with the vale. 
         private double _inkLevel;
         public double InkLevel {
@@ -27,7 +27,8 @@ namespace OPP_Practice
                 if(value>100)
                 {
                     _inkLevel = 100;
-                } else
+                } 
+                else
                 {
                     if(value<0)
                     {
@@ -44,24 +45,25 @@ namespace OPP_Practice
         {
             Brand = "Bic";
             InkColor = "Black";
-            InkLevel = 100;
-            MaxInk = 100;
-            HasLid = true;
+            InkLevel = 100; //Default at 100%
+            MaxInk = 10; // This one has the value of mL
+            HasLid = true; //true means it has cap on
         }
 
 
-        //Write() and Write(characterCount) - lower ink level according to the number of characters written,
-
         // assuming 10 characters for the version without parameters
         public void Write()
-        {
-            InkLevel -= 0.5;
+        {   
+            //Default 10 characters ink mL dropped at 0.5 mL
+            MaxInk -= 0.5;
+            InkLevel = (MaxInk/10)*100;
         }
 
         //One character equals 0.05mL of ink usage
         public void Write(double characterCount)
         {
-            characterCount -= 0.05 * characterCount;
+            MaxInk -= 0.05 * characterCount;
+            InkLevel = (MaxInk/10)*100;
         }
 
 
--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ namespace OPP_Practice
         {
             Pen pencil = new Pen();
             pencil.Write();
-            Console.WriteLine(pencil.InkLevel);
+            Console.WriteLine($"The brand of the pen is: {pencil.Brand}");
+            Console.WriteLine($"The colour of the pen is: {pencil.InkColor}");
+            Console.WriteLine($"Whether the pen has lid: {pencil.InkColor}");
+            Console.WriteLine($"The current ink level in mL is : {pencil.MaxInk} mL");
+            Console.WriteLine($"The current ink percentage left : {pencil.InkLevel} %");
 
         }
     }
