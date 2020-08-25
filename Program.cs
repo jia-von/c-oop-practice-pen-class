618876b86a15f07a4e0cf76307f10efad372f771
Jia <jiavon@ualberta.ca>
2020-08-25 16:02:15 -0600

    initial commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace OPP_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!  ");
         }
     }
 }
